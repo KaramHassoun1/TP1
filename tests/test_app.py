b8a90de4cca54d3bda7d1f72e783040f9a71ef48
KaramHassoun <mohamedkaram.hassoun@insat.ucar.tn>
2024-10-21 14:47:16 +0100

    Update test.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,7 @@
-def test_index(client):
-    response = client.get('/')
-    assert response.status_code == 200
+from app import app
+
+def test_hello_world():
+    with app.test_client() as client:
+        response = client.get('/')
+        assert response.status_code == 200
+        assert response.data == b'Hello, World! This is a basic Flask app....'
